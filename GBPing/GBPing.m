d40a23f55e25ea9849ae0d80df2cfcaf6469e340
V.Abdrakhmanov <vladimira@2kgroup.com>
2018-05-21 11:51:44 +0300

    Removed asserts.

--- GBPing/GBPing.m
+++ GBPing/GBPing.m
@@ -366,8 +366,11 @@ static NSTimeInterval const kDefaultTimeout =           2.0;
     enum { kBufferSize = 65535 };
     
     buffer = malloc(kBufferSize);
-    assert(buffer);
-    
+    if (buffer == nil) {
+        err = errno;
+        return;
+    }
+
     //read the data.
     addrLen = sizeof(addr);
     bytesRead = recvfrom(self.socket, buffer, kBufferSize, 0, (struct sockaddr *)&addr, &addrLen);
@@ -388,7 +391,10 @@ static NSTimeInterval const kDefaultTimeout =           2.0;
             NSMutableData *packet;
 
             packet = [NSMutableData dataWithBytes:buffer length:(NSUInteger) bytesRead];
-            assert(packet);
+            if (packet == nil) {
+                err = errno;
+                return;
+            }
 
             //complete the ping summary
             const struct ICMPHeader *headerPointer;
@@ -498,7 +504,8 @@ static NSTimeInterval const kDefaultTimeout =           2.0;
                 packet = [self pingPacketWithType:kICMPv6TypeEchoRequest payload:payload requiresChecksum:NO];
             } break;
             default: {
-                assert(NO);
+                err = errno;
+                return;
             } break;
         }
         
@@ -764,7 +771,6 @@ static uint16_t in_cksum(const void *buffer, size_t bufferLen)
             result = [self isValidPing6ResponsePacket:packet];
         } break;
         default: {
-            assert(NO);
             result = NO;
         } break;
     }
@@ -854,7 +860,7 @@ static uint16_t in_cksum(const void *buffer, size_t bufferLen)
     ICMPHeader *            icmpPtr;
     
     packet = [NSMutableData dataWithLength:sizeof(*icmpPtr) + payload.length];
-    assert(packet != nil);
+    if (packet == nil) { return nil; }
     
     icmpPtr = packet.mutableBytes;
     icmpPtr->type = type;
